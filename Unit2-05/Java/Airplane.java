ca427652dd3b9d179a44ab88ed977a41826ab27e
Evan Beaudoin <Evan Beaudoin>
2024-04-04 21:51:19 -0400

    Init

---
+++ Unit2-05/Java/Airplane.java
@@ -0,0 +1,40 @@
+/*
+* This class defines a string stack
+*
+* @author  Evan Beaudoin
+* @version 1.0
+* @since   2024-04-04
+*/
+
+
+/**
+ * This is the class.
+*/
+public class Airplane {
+    /**
+     * Adds strings to an array.
+     *
+     * @param array contains the array of strings
+    */
+    public int speed;
+
+    /**
+     * Returns the speed of the airplane value.
+     *
+     * @return the speed
+    */ 
+    public int getSpeed() {
+      return this.speed;
+    }
+
+    /**
+     * Sets the speed of the airplane.
+     *
+     * @param int sets the speed
+    */ 
+    public void setSpeed(int speed) {
+      this.speed = speed;
+    }
+
+}
+
